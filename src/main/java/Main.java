22cc1ee25441bd205ca72df7c7e79618b07bf160
Achilles-Lang <2743942427@qq.com>
2025-08-04 14:39:54 +0800

    fix the Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,24 +9,26 @@ public class Main {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            //1.在循环外部接受一次连接
+            clientSocket = serverSocket.accept();
+            //获取该连接的输入和输出流
+            OutputStream outputStream = clientSocket.getOutputStream();
+            InputStream inputStream = clientSocket.getInputStream();
 
-          while(clientSocket != null){
-              OutputStream outputStream = clientSocket.getOutputStream();
-              outputStream.write("+PONG\r\n".getBytes());
-              outputStream.flush();
-              clientSocket = serverSocket.accept();
-          }
+            //创建一个缓冲区来读取数据
+            byte[] buffer = new byte[1024];
+            //2.循环读取来自同一个客户端的数据
+            while(inputStream.read(buffer) != -1){
+                // 3.每当读取到数据，就发送一个 PONG 响应
+                outputStream.write("+PONG\r\n".getBytes());
+                outputStream.flush();
+            }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
