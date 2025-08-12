0dbf570e9014e969c686dc1b0ba6db72a5e2c6af
Achilles-Lang <2743942427@qq.com>
2025-08-12 21:41:20 +0800

    Query entries from stream

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,37 +7,20 @@ import java.net.Socket;
 
 public class Main {
   public static void main(String[] args){
-    System.out.println("Logs from your program will appear here!");
+      System.out.println("Logs from your program will appear here!");
 
-        ServerSocket serverSocket = null;
-        int port = 6379;
-        try {
-            serverSocket = new ServerSocket(port);
-            serverSocket.setReuseAddress(true);
-            CommandHandler commandHandler=new CommandHandler();
+      try (ServerSocket serverSocket = new ServerSocket(6379)) {
+          serverSocket.setReuseAddress(true);
+          CommandHandler commandHandler = new CommandHandler(); // 创建一个命令处理器
 
-            //主线程等待新连接
-            while(true){
-                //等待下一个客户端连接，这里会一直阻塞直到有客户端连接进来
-                Socket clientSocket=serverSocket.accept();
-                System.out.println("Accepted new connection from " + clientSocket.getRemoteSocketAddress());
-
-                //为进来的客户端创建一个线程来处理PING
-                Thread clientThread = new Thread(new ClientHandler(clientSocket, commandHandler));
-
-                //启动线程，让线程来响应PING
-                clientThread.start();
-            }
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (serverSocket != null) {
-              serverSocket.close();
-            }
-          } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
+          while (true) {
+              Socket clientSocket = serverSocket.accept();
+              // 将命令处理器传递给每个客户端线程
+              Thread clientThread = new Thread(new ClientHandler(clientSocket, commandHandler));
+              clientThread.start();
           }
-        }
+      } catch (IOException e) {
+          System.out.println("IOException: " + e.getMessage());
+      }
   }
 }
