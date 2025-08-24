33b34af5a572056752f81ed02cd7c2ecd61031f5
郎启龙 <2743942427@qq.com>
2025-08-24 19:01:58 +0800

    PING in subscribed mode

--- src/main/java/Commands/Impl/PingCommand.java
+++ src/main/java/Commands/Impl/PingCommand.java
@@ -24,25 +24,30 @@ public class PingCommand implements Command {
 
     @Override
     public Object execute(List<byte[]> args, OutputStream os) {
-        if(this.isClientSubscribed){
-            //订阅模式下
-            List<byte[]> response=new ArrayList<>();
-            response.add("pong".getBytes(StandardCharsets.UTF_8));
+        try {
+            if(this.isClientSubscribed){
+                //订阅模式下
+                List<byte[]> response=new ArrayList<>();
+                response.add("pong".getBytes(StandardCharsets.UTF_8));
 
-            if(args.isEmpty()){
-                //如果PING带有参数，则返回该参数，否则返回空字符串
-                response.add("".getBytes(StandardCharsets.UTF_8));
+                if(args.isEmpty()){
+                    //如果PING带有参数，则返回该参数，否则返回空字符串
+                    response.add("".getBytes(StandardCharsets.UTF_8));
+                } else {
+                    response.add(args.get(0));
+                }
+                return response;
             } else {
-                response.add(args.get(0));
+                //非订阅模式下
+                if(args.isEmpty()){
+                    return "PONG".getBytes(StandardCharsets.UTF_8);
+                } else {
+                    return args.get(0);
+                }
             }
-            return response;
-        } else {
-            //非订阅模式下
-            if(args.isEmpty()){
-                return "PONG".getBytes(StandardCharsets.UTF_8);
-            } else {
-                return args.get(0);
-            }
+        } finally {
+            this.isClientSubscribed=false;
         }
+
     }
 }
